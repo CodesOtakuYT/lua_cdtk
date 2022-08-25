9fe8af92ae97154a3008c9c424e5d28b0dde7b9c
Codotaku <88196711+CodesOtakuYT@users.noreply.github.com>
2022-08-25 03:23:16 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,23 @@
 local a = CDTK.vec_i128()
-a:fill(0, 10)
-a[5] = 1;
-print(a, a[5])
+local b = CDTK.vec_i64()
+local c = CDTK.vec_i32()
+local d = CDTK.vec_i16()
+local e = CDTK.vec_i8()
+local f = CDTK.vec_f64()
+local e = CDTK.vec_f32()
+
+a:range(1, 5, 2)
+a:range(5, 1, 1)
+a:fill(1, 5)
+a:fill(2, 3)
+
+local examples = {#a, a, a+a, a-a, a*a, a/a, a%a, a^a, a..a, a:min(), a:max(), a:product(), a:sum(), a == a, a > a, a < a, a >= a, a <= a}
+for i, v in ipairs(examples) do
+    print(i, v)
+end
+
+a:negate()
+a:push(10)
+a:fill(a:pop(), a:pop())
+a:range(a:pop(), a:pop(), a:pop())
+print(a)
